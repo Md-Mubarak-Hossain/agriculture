9bc0dc7513af19e94e917c4ac5c1606b57743966
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2023-03-02 18:19:09 +0600

    first commit

---
+++ js/agriculture.js
@@ -0,0 +1,7 @@
+const text1=document.getElementById('paragraph1')
+text1.innerHTML=`<div>Hello <span class="name">Afroja Akter</span>. She is a new learner of Computer Science.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas adipisci perferendis deserunt eaque asperiores. Laudantium ipsa illo sequi consequatur autem, harum dicta temporibus commodi? Laborum consequatur temporibus odit totam impedit! Numquam blanditiis dolorum alias accusamus ullam veniam ea minima eius!</div>`
+
+const text2=document.getElementById('paragraph2')
+text2.innerHTML=`<div>Hello <span class="name">Afroja Akter</span>. She is a new learner of Computer Science.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas adipisci perferendis deserunt eaque asperiores. Laudantium ipsa illo sequi consequatur autem, harum dicta temporibus commodi? Laborum consequatur temporibus odit totam impedit! Numquam blanditiis dolorum alias accusamus ullam veniam ea minima eius!</div>`
+
+
---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+const navbar=document.getElementById('nav')
+navbar.innerHTML=`<div class="nav">
+<a href="">Home</a>
+<a href="">login</a>
+<a href="">Register</a>
+</div>`
+
+function newCode(){
+    const newcode=document.getElementById('newcode')
+    newcode.innerHTML=`<div>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel dolorem cupiditate tenetur quam eum molestiae dignissimos sapiente aliquid voluptatum sint.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel dolorem cupiditate tenetur quam eum molestiae dignissimos sapiente aliquid voluptatum sint.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel dolorem cupiditate tenetur quam eum molestiae dignissimos sapiente aliquid voluptatum sint.</p>
+    </div>`  
+}
